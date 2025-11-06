9c91a263873fc42c0d4406047048555ca65facb5
carlos-hereee <97hernandez.c@gmail.com>
2025-11-06 15:32:54 -0600

    add custom scripts

---
+++ src/utils/data.js
@@ -0,0 +1,18 @@
+export const updateFileMessages = {
+  success: { message: message.modified, status: "success", error: null },
+  skipped: { message: message.skipped, status: "skipped", error: null },
+  notFound: { message: message.notFound, status: "skipped", error: null },
+  skippedDir: { message: message.skippedDir, status: "skipped", error: null },
+};
+
+export const errorMessage = (error, msg) => {
+  return { status: "error", error, message: msg };
+};
+
+export const requiredProps = {
+  pattern: { message: message.regexRequired, status: "missingProps", error: "missing" },
+  filePath: { message: message.filePath, status: "missingProps", error: "missing" },
+  cb: { message: message.cb, status: "missingProps", error: "missing" },
+  searchPaths: { message: message.searchPaths, status: "missingProps", error: "missing" },
+  target: { message: message.target, status: "missingProps", error: "missing" },
+};
